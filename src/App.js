25b79fd1983ef0d056e721b6bd989f072318c08d
RicardoPenaAgg <78938833+RicardoPenaAgg@users.noreply.github.com>
2021-02-16 17:48:11 -0600

    Create quote App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from "react";
 function App() {
+  const [quoteData,setQuoteData] = React.useState({})
+  const getQuote= async ()=>{
+  const quoteData = await fetch("http://staging.quotable.io/random")
+  return quoteData.json()
+  }
+  
+  const updateQuote=()=>{
+    getQuote().then((quote)=>{setQuoteData(quote)})
+  }
+  React.useEffect(updateQuote,[])
+
+  const encodedQuote = encodeURIComponent(quoteData.content + "\n - " + quoteData.author)// makes URL safe
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+    <div id="quote-box" >
+          <div id="text">
+              {quoteData.content}
+          </div>
+          <div id="author">
+              -{quoteData.author}
+          </div>
+          
+          
+            <button id="new-quote" onClick={updateQuote}>New Quote</button>
+          
+          <a id="tweet-quote" title="Tweet It Now" target="_blank" href={`https://twitter.com/intent/tweet?text=${encodedQuote}`}>
+            <span><img src="http://cdn.onlinewebfonts.com/svg/img_276960.png" alt="twitter icon"/></span>
+            </a>
+
+      
     </div>
-  );
+  )
 }
 
 export default App;
